34d0f06e3879619ed641e8ad192a0e54202d77f9
Zack Goodwin <46791356+GoodZombie@users.noreply.github.com>
2021-09-04 11:31:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 
 app.get('/', function (req, res) {
 
-    res.send('<h3><a href="https://github.com/GoodZombie/hello-heroku">Hello Node on Heroku</a></h3>');
-    res.send('<h3><a href="https://github.com/GoodZombie/hello-heroku/blob/main/README.md">Read Me</a></h3>');
+    res.send('<h1><a href="https://github.com/GoodZombie/hello-heroku">Hello Node on Heroku</a></h1><h3><a href="https://github.com/GoodZombie/hello-heroku/blob/main/README.md">Read Me</a></h3>');
 
 });
 
